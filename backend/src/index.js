3ba0444e289f61ba9728bb0e0f3e0cfc133b691f
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-05-29 12:32:53 -0300

    Criacao do primeiro usuario na tabela usuarios e tratamento da API para retornos em JSON

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const routes = require('./routes.js')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(routes)
+app.use(bodyParser.json()) // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })) // support encoded bodies
+app.use('/', routes)
 
 app.listen(3333)
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const crypto = require('crypto')
+const connection = require('./database/conexao')
 
 const routes = express.Router()
 
-routes.use(express.json())
-
-routes.get('/users', (request, response) => {
-  return response.send({ Projeto: 'TCC', Aluno: 'Jorge de Almeida Bastos Jr' })
+routes.post('/usuarios', async (request, response) => {
+  //Desestruturação para pegar os dados em variaveis separadas
+  const { nome, classificacao, email, whatsapp } = request.body
+  //Gera 4 bytes de caracteres HEX (números e letras) que serão usados como ID
+  const id = crypto.randomBytes(4).toString('HEX')
+  //Insere os dados armazenados nas variaveis nas colunas da tabela usuarios
+  await connection('usuarios').insert({
+    id,
+    nome,
+    classificacao,
+    email,
+    whatsapp
+  })
+  return response.json({ id })
 })
 
 module.exports = routes
