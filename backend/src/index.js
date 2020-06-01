fff1a8909292438f1b253b1251cc4998b31716a9
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-06-01 12:04:19 -0300

    Instalação do módulo CORS para melhorar a segurança do backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const routes = require('./routes.js')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
+
+// Permite que qualquer aplicacao acesse o backend (API)
+app.use(cors())
+
+// Permite que apenas a aplicação frontend acesse o backend (API)
+/*
+app.use(cors({
+ origin: 'http://linkDaAppFrontEnd.com.br 
+}))
+*/
 
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: false })) // support encoded bodies
