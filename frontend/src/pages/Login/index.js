a5a35d27adcc0f1d6c3c169453f1e235c7a80779
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-06-04 15:32:42 -0300

    Login de usuario cadastrado no backend e id e nome do usuario armazenados em localstorage

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import './styles.css'
-import { Link } from 'react-router-dom'
-import brainstorm from '../../assets/brainstorm.svg'
-import brasaoUFSC from '../../assets/brasao_site_ufsc.svg'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 
+import brainstorm from '../../assets/brainstorm.svg'
+import brasaoUFSC from '../../assets/brasao_site_ufsc.svg'
+import api from '../../services/api'
+
+import './styles.css'
+
 export default function Login() {
+  const [ id, setId ] = useState('')
+  const historico = useHistory()
+
+  async function fazerLogin(e) {
+    e.preventDefault()
+
+    try {
+      const response = await api.post('/sessions', { id })
+      localStorage.setItem('usuarioID', id)
+      localStorage.setItem('usuarioNome', response.data.nome)
+      historico.push('/perfil')
+    } catch (error) {
+      alert('Falha no login, tente novamente')
+    }
+  }
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={brasaoUFSC} alt="Brasão da UFSC" />
-        <form>
+        <form onSubmit={fazerLogin}>
           <h1>Faça seu login aqui!</h1>
-          <input placeholder="Sua ID" />
+          <input value={id} onChange={(e) => setId(e.target.value)} placeholder="Sua ID" />
           <button className="button" type="submit">
             Entrar
           </button>
