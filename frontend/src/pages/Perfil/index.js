9ad13c94ff73cc95de08d7a0dbdad85030760768
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-06-04 15:44:49 -0300

    Exibir valores de cada proposta pertencente ao usuário logado

--- frontend/src/pages/Perfil/index.js
+++ frontend/src/pages/Perfil/index.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import './styles.css'
 import brasaoUFSC from '../../assets/brasao_site_ufsc.svg'
+import api from '../../services/api'
 
 export default function Perfil() {
+  const usuarioID = localStorage.getItem('usuarioID')
+  const usuarioNome = localStorage.getItem('usuarioNome')
+  const [ propostas, setPropostas ] = useState([])
+
+  useEffect(
+    () => {
+      api
+        .get('perfil', {
+          headers: {
+            Authorization: usuarioID
+          }
+        })
+        .then((response) => {
+          setPropostas(response.data)
+        })
+    },
+    [ usuarioID ]
+  )
+
   return (
     <div className="perfil-container">
       <header>
         <img src={brasaoUFSC} alt="Brasão da UFSC" />
-        <span>Bem vindo, Jorge Bastos</span>
+        <span>Bem vindo, {usuarioNome}</span>
         <Link className="button" to="/propostas/nova">
           Cadastrar nova Proposta
         </Link>
@@ -21,38 +41,17 @@ export default function Perfil() {
 
       <h1>Casos Cadastrados</h1>
       <ul>
-        <li>
-          <strong>PROPOSTA:</strong>
-          <p>Caso teste</p>
-          <strong>DESCRIÇÃO:</strong>
-          <button>
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>PROPOSTA:</strong>
-          <p>Caso teste</p>
-          <strong>DESCRIÇÃO:</strong>
-          <button>
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>PROPOSTA:</strong>
-          <p>Caso teste</p>
-          <strong>DESCRIÇÃO:</strong>
-          <button>
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>{' '}
-        <li>
-          <strong>PROPOSTA:</strong>
-          <p>Caso teste</p>
-          <strong>DESCRIÇÃO:</strong>
-          <button>
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
+        {propostas.map((proposta) => (
+          <li key={proposta.id}>
+            <strong>PROPOSTA:</strong>
+            <p>{proposta.titulo}</p>
+            <strong>DESCRIÇÃO:</strong>
+            <p>{proposta.descricao}</p>
+            <button>
+              <FiTrash2 size={20} color="#a8a8b3" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   )
